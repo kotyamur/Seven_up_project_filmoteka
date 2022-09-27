f271416ee895fc4280ce0ca9f2193c37219641f4
Mikhaylo Pobochikh <pma.ua01@gmail.com>
2022-09-27 18:39:59 +0300

    end

--- src/js/markupFilms.js
+++ src/js/markupFilms.js
@@ -9,15 +9,15 @@ export default function markupFilms(results) {
             <div class="poster__thumb">
               <img src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${original_title}" class="poster__foto" />
             </div>
-              <div class="postrer__title">
-                <p class="poster__text">${title}</p>
+              <div class="poster__about">
+                <p class="poster__title">${title}</p>
+                  <div class="poster__description">
+                <p class="poster__genre">Drama</p>
+                <p class="poster__year">${year}</p>
+                <p class="poster__rating"><span class="poster__rating__text">${vote_average}</span></p>
+                </div>
               </div>
 
-            <div class="poster__description">
-              <p class="poster__genre">Drama</p>
-              <p class="poster__year">${year}</p>
-              <p class="poster__rating"><span class="poster__rating__text">${vote_average}</span></p>
-            </div>
           </a>
         </li>`;
       }
