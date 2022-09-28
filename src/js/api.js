9db7edf6c28fe0b94145e1d4b809817694059834
Mikhaylo Pobochikh <pma.ua01@gmail.com>
2022-09-28 16:39:01 +0300

    fix_objectfit+add genre

--- src/js/api.js
+++ src/js/api.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 const APIKEY = 'a97f5a48286213b4292b81d1cb5cf0d2';
+const URL = 'https://api.themoviedb.org/3/';
 
 export default class Api {
   constructor() {
     this.query = '';
     this.totalResult = '';
     this.totlaPages = '';
+    this.genre = [];
   }
 
   async search() {
@@ -15,14 +17,31 @@ export default class Api {
     });
 
     const api = axios.create({
-      baseURL: 'https://api.themoviedb.org/3/',
+      baseURL: URL,
     });
+
     const result = await api.get(`search/movie?${searchParams}`);
 
     this.totalResult = result.data.total_results;
     this.totlaPages = result.data.total_pages;
 
     return result.data.results;
+  }
+
+  async findGenre() {
+    const api = axios.create({
+      baseURL: URL,
+    });
+    const searchParamsKey = new URLSearchParams({
+      api_key: APIKEY,
+    });
+    const apiGenre = axios.create({
+      baseURL: 'URL',
+    });
+
+    const genre = await api.get(`genre/movie/list?${searchParamsKey}`);
+    this.genre = genre.data.genres;
+    return genre.data.genres;
   }
 
   get searchQuery() {
--- src/js/markupFilms.js
+++ src/js/markupFilms.js
@@ -1,18 +1,45 @@
+import { genre } from './genre';
 export default function markupFilms(results) {
   return results
     .map(
-      ({ poster_path, original_title, vote_average, release_date, title }) => {
-        const year = String(release_date).slice(0, 4);
-
+      ({
+        poster_path,
+        original_title,
+        vote_average,
+        release_date,
+        title,
+        genre_ids,
+      }) => {
+        let arrayGenre = [];
+        let url = 'https://image.tmdb.org/t/p/w500/';
+        let year = String(release_date).slice(0, 4);
+        if (poster_path === null) {
+          url = 'https://i.postimg.cc/NF6QdzcX/no.jpg';
+          poster_path = '';
+        }
+        if (release_date === '') {
+          console.log('no data');
+          year = 'no information';
+        }
+        if ((genre_ids = [])) {
+          arrayGenre.push('No information');
+        }
+        for (const gen of genre) {
+          for (const id of genre_ids) {
+            if (id === gen.id) {
+              arrayGenre.push(gen.name);
+            }
+          }
+        }
         return `<li class="films__item">
           <a href="" class="films__item-link">
             <div class="poster__thumb">
-              <img src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${original_title}" class="poster__foto" />
+              <img src="${url}${poster_path}" alt="${original_title}" class="poster__foto" />
             </div>
               <div class="poster__about">
                 <p class="poster__title">${title}</p>
                   <div class="poster__description">
-                <p class="poster__genre">Drama</p>
+                <p class="poster__genre">${arrayGenre}</p>
                 <p class="poster__year">${year}</p>
                 <p class="poster__rating"><span class="poster__rating__text">${vote_average}</span></p>
                 </div>
