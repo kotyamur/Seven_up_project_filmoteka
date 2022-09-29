892e4b85f886e1103e5dd8c0724b3839495969cb
grant956 <68757554+Psihodel666@users.noreply.github.com>
2022-09-29 23:46:56 +0300

    fix title

--- src/js/sort-top-film.js
+++ src/js/sort-top-film.js
@@ -1,26 +1,41 @@
+import axios from 'axios';
 import markupFilms from './markupFilms';
+import renderPagination from './pagination'
 import Api from './api';
-import { renderUi } from './renderUi';
-import { renderPagination } from './pagination';
 
+
+let pageNumber = 1;
 const refs = {
   filmLsit: document.querySelector('.films'),
 };
 
-const api = new Api();
+const API_URL = 'https://api.themoviedb.org/3';
+const API_KEY = 'a97f5a48286213b4292b81d1cb5cf0d2';
+
+  
 
 async function sortTopFilmh() {
-  try {
+  try{
     refs.filmLsit.innerHTML = '';
-    const response = await api.searchPopular();
-    const sortArrFilm = response.sort(
-      (firstReting, secondRating) =>
-        secondRating.vote_average - firstReting.vote_average
-    );
-    renderUi(sortArrFilm);
-    renderPagination(api, renderUi, 'searchPopular');
-  } catch (error) {
-    console.log(error);
+    const response = await axios.get(`${API_URL}/trending/movie/week?api_key=${API_KEY}`); 
+    const arrFilm = response.data.results
+
+
+    const sortArrFilm = arrFilm.sort( (firstReting, secondRating) =>  secondRating.vote_average - firstReting.vote_average)
+    
+   
+   renderTopFilm(sortArrFilm)
+   
   }
+  catch{
+    console.log(error)
+  }  
+  }
+
+
+function renderTopFilm(sortArrFilm){
+  refs.filmLsit.insertAdjacentHTML('beforeend', markupFilms(sortArrFilm))
 }
-sortTopFilmh();
+
+
+  sortTopFilmh()
