9cc2715773bfc38887a2a88176428ca14d017225
Vladimir Ponomarov <ripper3061@gmail.com>
2022-09-29 14:36:37 +0300

    фото + спрайты иконок

--- src/js/students-modal.js
+++ src/js/students-modal.js
@@ -58,20 +58,20 @@ const makeCard = ({ photo, name, description }) => {
         </p>
          <ul class="social-links list">
                 <li class="social-links__item">
-                  <a class="social-links__link" href="">
-                  <svg class="social-links__icon" width="20" height="20">
-                      <use href="images/icons.svg#instagram"></use></svg
-                  >
+                  <a class="social-links__link instagram-icon" href="">
                   </a>
                 </li>
                 <li class="social-links__item">
-                  <a class="social-links__link" href="">🕊</a>
+                  <a class="social-links__link twitter-icon" href="">
+                  </a>
                 </li>
                 <li class="social-links__item">
-                  <a class="social-links__link" href="">🐋</a>
+                  <a class="social-links__link facebook-icon" href="">
+                  </a>
                 </li>
                 <li class="social-links__item">
-                  <a class="social-links__link" href="">🦖</a>
+                  <a class="social-links__link linkedin-icon" href="">
+                  </a>
                 </li>
           </ul>
       </div>
