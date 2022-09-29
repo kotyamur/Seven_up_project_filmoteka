339b7fdf1e57d68000762c76f124b1f4c1312370
CrocodileGenna <yuramus1998@gmail.com>
2022-09-29 13:50:44 +0300

    altr loader

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,10 +1,33 @@
-import {Spinner} from 'spin.js';
+// import {Spinner} from 'spin.js';
 
 
-function loader(elements){
-  const target = document.getElementById('foo');
-  const spinner = new Spinner().spin(target);
-  target.appendChild(spinner.el);
-  return spinner
-}
-loader()
+
+// let target = document.getElementById('foo');
+// const spinner = new Spinner().spin(target);
+// window.onload = async function(){
+//   try{
+//   await new Spinner().stop(target);
+//   console.log(spinner)
+//   }catch (error) {
+//     console.log(error);
+//   }
+// }
+document.addEventListener('DOMContentLoaded', () => {
+
+  const mediaFiles = document.querySelectorAll('body');
+  let i = 0
+
+  Array.from(mediaFiles).forEach((file, index) => {
+      file.onload = () => {
+          i++
+
+          // percents.innerHTML = ((i * 100) / mediaFiles.length).toFixed(1)
+
+          if(i === mediaFiles.length) {
+              preloader.classList.add('preloader--hide')
+              // percents.innerHTML = 100
+          }
+      }
+  })
+
+})
