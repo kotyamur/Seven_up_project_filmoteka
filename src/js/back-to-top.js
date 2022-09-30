e1d8061764345315d2f5d6a09c9bae3ee5de6bc2
grant956 <68757554+Psihodel666@users.noreply.github.com>
2022-09-30 17:42:07 +0300

    delate space

--- src/js/back-to-top.js
+++ src/js/back-to-top.js
@@ -1,43 +1,27 @@
 let intervalId = 0; 
 const scrollButton = document.querySelector('.back-to-top');
-
 function scrollStep() {
-    
     if (window.pageYOffset === 0) {
         clearInterval(intervalId);
     }
     window.scroll(0, window.pageYOffset - 50);
 }
-
 function scrollToTop() {
-    
     intervalId = setInterval(scrollStep, 16.66);
 }
-
-
-function scrollHiden(e){
-
+function scrollHiden(){
     if(window.scrollY < 400){
         scrollButton.style.display = 'none'
     }else{
         scrollButton.style.display = ''
     }
-
-    if(window.scrollY < 700){
-       
+    if(window.scrollY < 700){  
         scrollButton.classList.add('hiden')
         console.log('asas')
     }else{
         scrollButton.classList.remove('hiden')
-       
     }
-
-   
-    
 }
-
-
-
 scrollButton.addEventListener('click', scrollToTop);
 window.addEventListener('scroll', scrollHiden)
 
