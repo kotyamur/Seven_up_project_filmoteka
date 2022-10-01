c56ecf065f372198220d53dc2790286321503582
Vladimir Ponomarov <ripper3061@gmail.com>
2022-10-01 20:25:55 +0300

    add button

--- src/library.js
+++ src/library.js
@@ -3,5 +3,6 @@ import './js/constants';
 import './js/elements';
 import './js/students-modal';
 import './js/dark-theme';
-import './js/renderLibrary'
+import './js/renderLibrary';
 import './js/api_movie';
+import './js/back-to-top';
