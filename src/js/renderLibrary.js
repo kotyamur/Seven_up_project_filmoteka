436f01ff83ab0f9bbe439bde7efe884925446e63
Kateryna Kleshch <ketrins1993@gmail.com>
2022-10-01 18:43:35 +0300

    add render and pagination on each page

--- src/js/renderLibrary.js
+++ src/js/renderLibrary.js
@@ -1,45 +1,46 @@
 import markupFilms from './markupFilms';
 import { renderLibraryPagination } from './pagination';
 const refs = {
-    btnWatched : document.querySelector('.watched'),
-    btnQueue : document.querySelector('.queue'),
-    films : document.querySelector('.films'),
-    plug : document.querySelector('.plug')
-}
+  btnWatched: document.querySelector('.watched'),
+  btnQueue: document.querySelector('.queue'),
+  films: document.querySelector('.films'),
+  plug: document.querySelector('.plug'),
+};
 
-
-refs.btnWatched.addEventListener('click', getWatched)
-refs.btnQueue.addEventListener('click', getQueue)
+refs.btnWatched.addEventListener('click', getWatched);
+refs.btnQueue.addEventListener('click', getQueue);
 
 getWatched();
-function getWatched(){
+function getWatched() {
+  refs.films.innerHTML = '';
+  const parsItem = JSON.parse(localStorage.getItem('watched'));
+  if (parsItem !== null && parsItem.length !== 0) {
+    refs.plug.style.display = 'none';
+    refs.films.innerHTML = markupFilms(parsItem.slice(0, 20));
+    renderLibraryPagination('watched', films => {
+      refs.films.innerHTML = markupFilms(films);
+    });
+  } else {
+    refs.plug.style.display = 'flex';
     refs.films.innerHTML = '';
-    const parsItem = JSON.parse(localStorage.getItem('watched'))
-    if(parsItem !== null && parsItem.length !== 0){
-        refs.plug.style.display = 'none'
-        refs.films.insertAdjacentHTML('beforeend', markupFilms(parsItem))
-        renderLibraryPagination('watched');
-    }else{
-        refs.plug.style.display = 'flex'
-        refs.films.innerHTML = '';
-    }
-    refs.btnQueue.classList.remove('is-active')
-    refs.btnWatched.classList.add('is-active')
+  }
+  refs.btnQueue.classList.remove('is-active');
+  refs.btnWatched.classList.add('is-active');
 }
 
-function getQueue(){
+function getQueue() {
+  refs.films.innerHTML = '';
+  const parsItem = JSON.parse(localStorage.getItem('queue'));
+  if (parsItem !== null && parsItem.length !== 0) {
+    refs.plug.style.display = 'none';
+    refs.films.innerHTML = markupFilms(parsItem.slice(0, 20));
+    renderLibraryPagination('queue', films => {
+      refs.films.innerHTML = markupFilms(films);
+    });
+  } else {
+    refs.plug.style.display = 'flex';
     refs.films.innerHTML = '';
-    const parsItem = JSON.parse(localStorage.getItem('queue'))
-    if(parsItem !== null && parsItem.length !== 0){
-        refs.plug.style.display = 'none'
-        refs.films.insertAdjacentHTML('beforeend', markupFilms(parsItem))
-        renderLibraryPagination('queue');
-    }else{
-        refs.plug.style.display = 'flex'
-        refs.films.innerHTML = '';
-    }
-    refs.btnQueue.classList.add('is-active')
-    refs.btnWatched.classList.remove('is-active')
+  }
+  refs.btnQueue.classList.add('is-active');
+  refs.btnWatched.classList.remove('is-active');
 }
-
-
